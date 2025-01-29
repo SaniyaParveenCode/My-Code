0239fef59c9f70e9654bf5e8110c30f361074392
SaniyaParveenCode <168137306+SaniyaParveenCode@users.noreply.github.com>
2025-01-29 12:07:49 +0530

    Time: 4 ms (44.69%), Space: 19.4 MB (9.32%) - LeetHub

--- 0946-smallest-range-ii/0946-smallest-range-ii.cpp
+++ 0946-smallest-range-ii/0946-smallest-range-ii.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    int smallestRangeII(vector<int>& nums, int k) {
-        int n = nums.size();
-        sort(nums.begin() , nums.end());
+int smallestRangeII(vector<int>& nums, int k) {
+int n = nums.size();
+sort(nums.begin() , nums.end());
+int min_ = INT_MAX;
+int max_ = INT_MIN;
+int res = nums[n-1] - nums[0];
+int bestL = nums[0]+k;
+int bestR = nums[(n-1)]-k;
 
-        if(n==0 || n==1 )
-        return 0;
-        
-        int max_ = nums[n-1];
-        int min_ = nums[0];
-        int res = max_ -min_;
 
-        for(int i=1;i<n;i++) {
-            max_ = max(nums[i-1] + k , nums[n-1] - k);
-            min_ = min(nums[i] - k , nums[0] + k);
-            res = min(res , max_ - min_);
-        }
-        return res;
-    }
+for(int i=0;i<n-1;i++) {
+    int max_ = max(nums[n-1] - k, nums[i] +k); 
+    int min_ = min(nums[0] + k, nums[i+1] - k);
+    res = min(res , max_ - min_);
+}
+return res;
+
+}
 };
