f50cfa5c61a5d7b7608efad970a39b7e4f261462
SaniyaParveenCode <168137306+SaniyaParveenCode@users.noreply.github.com>
2025-01-29 13:03:41 +0530

    Time: 0 ms (100%), Space: 12.3 MB (38.27%) - LeetHub

---
+++ 0274-h-index/0274-h-index.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        sort(citations.begin() , citations.end());
+        int n = citations.size();
+        int hIndex = 0;
+        for(int i=0;i<n;i++) {
+            if(citations[i] >= n-i){
+            hIndex = n-i;
+            break;
+        }
+    }
+return hIndex;
+        
+    }
+};
