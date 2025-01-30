84a5e90b1bf21039e437498694b2a373be1cb44b
SaniyaParveenCode <168137306+SaniyaParveenCode@users.noreply.github.com>
2025-01-30 11:07:13 +0530

    Time: 76 ms (35.57%), Space: 168.4 MB (5.34%) - LeetHub

--- 2165-plates-between-candles/2165-plates-between-candles.cpp
+++ 2165-plates-between-candles/2165-plates-between-candles.cpp
@@ -1,62 +1,47 @@
 class Solution {
 public:
+
+void number_of_plates(string s, vector<int>&prefsum_plates){
+   int cnt=0; 
+for(int i=0;i<s.size();i++)
+{
+    if(s[i]=='*')cnt++;
+    prefsum_plates[i]=cnt;
+}
+}
+
+void vector_pref_candle(string&s,vector<int>&pref_candle){
+    int last_idx=-1;
+    for(int i=0;i<s.size();i++){
+        if(s[i]=='|')last_idx=i;
+        pref_candle[i]=last_idx;
+    }
+}
+void vector_suff_candle(string&s,vector<int>&suff_candle){
+    int last_idx=-1;
+    for(int i=s.size()-1;i>=0;i--){
+        if(s[i]=='|')last_idx=i;
+        suff_candle[i]=last_idx;
+    }
+}
     vector<int> platesBetweenCandles(string s, vector<vector<int>>& queries) {
-        
-        int n = s.size();
-        
-        //Plate Prefix Sum  
-        vector<int> total(n);
-        if(s[0] == '*') 
-            total[0] = 1;
-        
-        for(int i=1; i<n; i++)
-        {
-            if(s[i] == '*')
-                total[i] = total[i-1] + 1;
-            else
-                total[i] = total[i-1];
-        }
-        
-        //Candle - Leftmost Nearest Prefix Sum 
-        vector<int> left(n);
-        if(s[0] == '|')
-            left[0] = 0;
-        else
-            left[0] = -1;
-        for(int i=1; i<n; i++)
-        {
-            if(s[i] == '|')
-                left[i] = i;
-            else 
-                left[i] = left[i-1];
-        }
-        
-        //Candle - Rightmost Nearest Prefix Sum 
-        vector<int> right(n);
-        if(s[n-1] == '|')
-            right[n-1] = n-1;
-        else
-            right[n-1] = -1;
-        for(int i=n-2; i>=0; i--)
-        {
-            if(s[i] == '|')
-                right[i] = i;
-            else
-                right[i] = right[i+1];
-        }
-        
-        int q;
-        q = queries.size();
-        vector<int> ans;
-        for(int i=0; i<queries.size(); i++){
-            int l = right[queries[i][0]];
-            int r = left[queries[i][1]];
-            if(l == -1 || r==-1 || l>=r)
-            {
-                ans.push_back(0);
-                continue;
+        vector<int>prefsum_plates(s.size(),0);
+        vector<int>ans;
+        vector<int>pref_candle(s.size(),0);
+        vector<int>suff_candle(s.size(),0);
+        vector_pref_candle(s,pref_candle);
+        vector_suff_candle(s,suff_candle);
+        number_of_plates(s,prefsum_plates);
+        for(auto q:queries){
+            int start=q[0];
+            int end=q[1];
+            int left=suff_candle[start];
+            int right=pref_candle[end];
+            if(left==-1||left==-1||right<=left)ans.push_back(0);
+            else { 
+               if(left!=0) ans.push_back(prefsum_plates[right]-prefsum_plates[left-1]);
+               else ans.push_back(prefsum_plates[right]);
             }
-            ans.push_back(total[r]-total[l]);
         }
         return ans;
     }
