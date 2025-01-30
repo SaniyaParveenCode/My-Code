5593c1bc246625887b328460b142dc7603d4943f
SaniyaParveenCode <168137306+SaniyaParveenCode@users.noreply.github.com>
2025-01-30 22:21:13 +0530

    Time: 0 ms (100%), Space: 12.9 MB (18.91%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
-    int hIndex(vector<int>& citations) {
-        sort(citations.begin() , citations.end());
-        int n = citations.size();
-        int hIndex = 0;
-        for(int i=0;i<n;i++) {
-            if(citations[i] >= n-i){
-            hIndex = n-i;
-            break;
+int hIndex(vector<int>& citations) {
+vector<int> arr(citations.size() + 1, 0);
+        int sum = 0;
+        for (int i = 0; i < citations.size(); i++) {
+            if (citations[i] > citations.size())
+                arr[arr.size() - 1]++;
+            else
+                arr[citations[i]]++;
         }
-    }
-return hIndex;
-        
+        for (int i = arr.size() - 1; i >= 0; i--) {
+            sum += arr[i];
+            if (sum >= i)
+                return i;
+        }
+        return 0;
     }
 };
