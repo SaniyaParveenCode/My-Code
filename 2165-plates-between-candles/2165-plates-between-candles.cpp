d67affb138e57f23f7932692157323d31ff74e0e
SaniyaParveenCode <168137306+SaniyaParveenCode@users.noreply.github.com>
2025-01-30 11:01:15 +0530

    Time: 87 ms (29.84%), Space: 167.8 MB (7.91%) - LeetHub

--- 2165-plates-between-candles/2165-plates-between-candles.cpp
+++ 2165-plates-between-candles/2165-plates-between-candles.cpp
@@ -2,31 +2,41 @@ class Solution {
 public:
     vector<int> platesBetweenCandles(string s, vector<vector<int>>& queries) {
         int n = s.size();
-        int count = 0;
-        vector<int>prefixsum(n , 0);
-        // to store index of candles in string
-        vector<int>cp;
-        vector<int>ans;
-
-        for(int i=0;i<n;i++) {
-            if(s[i]=='*') 
-            count++;
-            if(s[i]=='|')
-            cp.push_back(i);
-            prefixsum[i] = count;
-            // This runs for every `i`, regardless of `s[i]`
+		// Stores the index of the candle present to the left of the current index
+        vector<int> left(n, -1);
+		// Stores the index of the candle present to the right of the current index
+        vector<int> right(n, -1);
+		// Prefix sum of plates
+        vector<int> count(n, 0);
+        
+		count[0] = (s.front() == '*' ? 1 : 0);
+        left[0] = (s.front() == '|' ? 0 : -1);
+        right[n-1] = (s.back() == '|' ? n-1 : -1);
+        
+        int start = 1, end = n-2;
+        
+		// Calculating indices and prefix sum
+        while(start < n){
+            count[start] = (s[start] == '*' ? 1 : 0) + count[start-1];
+            left[start] = (s[start] == '|' ? start : left[start-1]);
+            right[end] = (s[end] == '|' ? end : right[end+1]);    
+            start++;
+            end--;
         }
-        for(int i=0;i<queries.size();i++) {
-            int l = lower_bound(cp.begin() , cp.end() ,  queries[i][0]) - cp.begin();
-            int r = upper_bound(cp.begin(),cp.end(), queries[i][1])-cp.begin();
-            if (l >= cp.size() || r <= 0 || r > cp.size() || prefixsum[cp[r-1]] - prefixsum[cp[l]] < 0)
-
-            ans.push_back(0);
-            else ans.push_back(prefixsum[cp[r-1]]-prefixsum[cp[l]]);
-      }
-      return ans;  
+        
+        vector<int> ans;
+        for(auto it: queries){
+            int l = right[it[0]], r = left[it[1]];
+			
+			// No proper candle boundaries
+            if(l == -1 || r == -1 || l >= r){
+                ans.push_back(0);
+            }
+            else{
+                ans.push_back(count[r]-count[l]);
+            }
+        }
+        
+        return ans;
     }
 };
-
-
-        
