eb9ffc09958453c37c0114767a3d0e487097ff53
SaniyaParveenCode <168137306+SaniyaParveenCode@users.noreply.github.com>
2025-01-30 11:18:09 +0530

    Time: 27 ms (62.44%), Space: 88.1 MB (74.63%) - LeetHub

---
+++ 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maximumCandies(vector<int>& candies, long long k) {
+        int left = 1;
+        int right = *max_element(candies.begin(), candies.end()) + 1; // Binary search range
+        while (left < right) {
+            long long sum = 0;
+            int mid = (left + right) / 2; // Midpoint
+            for (auto i : candies) {
+                sum += i / mid; // Calculate total children satisfied with mid candies each
+            }
+            if (k > sum) { // If fewer children are satisfied
+                right = mid; // Move left
+            } else {
+                left = mid + 1; // Move right
+            }
+        }
+        return left - 1; // Result is left - 1
+    }
+};
