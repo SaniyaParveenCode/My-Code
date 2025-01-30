267ba6af072eec504b09a0fea3259a8fbe49d31b
SaniyaParveenCode <168137306+SaniyaParveenCode@users.noreply.github.com>
2025-01-30 11:02:08 +0530

    Time: 13 ms (81.62%), Space: 149.8 MB (42.69%) - LeetHub

--- 2165-plates-between-candles/2165-plates-between-candles.cpp
+++ 2165-plates-between-candles/2165-plates-between-candles.cpp
@@ -1,42 +1,63 @@
 class Solution {
 public:
     vector<int> platesBetweenCandles(string s, vector<vector<int>>& queries) {
+        
         int n = s.size();
-		// Stores the index of the candle present to the left of the current index
-        vector<int> left(n, -1);
-		// Stores the index of the candle present to the right of the current index
-        vector<int> right(n, -1);
-		// Prefix sum of plates
-        vector<int> count(n, 0);
         
-		count[0] = (s.front() == '*' ? 1 : 0);
-        left[0] = (s.front() == '|' ? 0 : -1);
-        right[n-1] = (s.back() == '|' ? n-1 : -1);
+        //Plate Prefix Sum  
+        vector<int> total(n);
+        if(s[0] == '*') 
+            total[0] = 1;
         
-        int start = 1, end = n-2;
-        
-		// Calculating indices and prefix sum
-        while(start < n){
-            count[start] = (s[start] == '*' ? 1 : 0) + count[start-1];
-            left[start] = (s[start] == '|' ? start : left[start-1]);
-            right[end] = (s[end] == '|' ? end : right[end+1]);    
-            start++;
-            end--;
+        for(int i=1; i<n; i++)
+        {
+            if(s[i] == '*')
+                total[i] = total[i-1] + 1;
+            else
+                total[i] = total[i-1];
         }
         
+        //Candle - Leftmost Nearest Prefix Sum 
+        vector<int> left(n);
+        if(s[0] == '|')
+            left[0] = 0;
+        else
+            left[0] = -1;
+        for(int i=1; i<n; i++)
+        {
+            if(s[i] == '|')
+                left[i] = i;
+            else 
+                left[i] = left[i-1];
+        }
+        
+        //Candle - Rightmost Nearest Prefix Sum 
+        vector<int> right(n);
+        if(s[n-1] == '|')
+            right[n-1] = n-1;
+        else
+            right[n-1] = -1;
+        for(int i=n-2; i>=0; i--)
+        {
+            if(s[i] == '|')
+                right[i] = i;
+            else
+                right[i] = right[i+1];
+        }
+        
+        int q;
+        q = queries.size();
         vector<int> ans;
-        for(auto it: queries){
-            int l = right[it[0]], r = left[it[1]];
-			
-			// No proper candle boundaries
-            if(l == -1 || r == -1 || l >= r){
+        for(int i=0; i<queries.size(); i++){
+            int l = right[queries[i][0]];
+            int r = left[queries[i][1]];
+            if(l == -1 || r==-1 || l>=r)
+            {
                 ans.push_back(0);
+                continue;
             }
-            else{
-                ans.push_back(count[r]-count[l]);
-            }
+            ans.push_back(total[r]-total[l]);
         }
-        
         return ans;
     }
 };
