424bdfe767ae30c4d54f82632e71570e19ca82a1
SaniyaParveenCode <168137306+SaniyaParveenCode@users.noreply.github.com>
2025-01-30 22:09:28 +0530

    Time: 26 ms (35.49%), Space: 17.5 MB (35.36%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>>data;
+        sort(nums.begin(),nums.end());
         int n = nums.size();
-
-        vector<vector<int>> ans;
-        sort(nums.begin(), nums.end());
-
-        set<vector<int>> uniqueSets;
-
-        for(int i = 0; i < n; i++){
-            for(int j = i + 1; j < n; j++){
-                int k = j + 1;
-                int l = n - 1;
-
-                while(k < l){
-                    long long sum = static_cast<long long>(nums[i]) + nums[j] + nums[k] + nums[l];
-
-                    if(sum == target){
-                        vector<int> quadruplet = {nums[i], nums[j], nums[k], nums[l]};
-                        uniqueSets.insert(quadruplet);
-                        k++;
-                        l--;
+        for(int i=0; i<nums.size();i++){
+            if(i>0 && nums[i]==nums[i-1])continue;
+            for(int j=i+1;j<nums.size();j++){
+                if(j>i+1 && nums[j]==nums[j-1] )continue;
+                int k= j+1;
+                int l=n-1;
+                while(k<l){
+                    long long sum=nums[i];
+                    sum+=nums[j];
+                    sum+=nums[k];
+                    sum+=nums[l];
+                    if(sum==target){
+                        data.push_back({nums[i],nums[j],nums[k],nums[l]});
+                        k++;l--;
+                        while(k<l&& nums[k]==nums[k-1])k++;
+                        while(k<l&& nums[l]==nums[l+1])l--;
                     }
-                    else if(sum < target) k++;
-                    else if(sum > target) l--;
+                    else if(sum<target)k++;
+                    else l--;
                 }
             }
         }
-        
-        ans.assign(uniqueSets.begin(), uniqueSets.end());
-        return ans;
+        return data;
     }
 };
