91f21059d1609658a01802f12e83785aa17689b0
SaniyaParveenCode <168137306+SaniyaParveenCode@users.noreply.github.com>
2025-01-31 07:55:40 +0530

    Time: 4 ms (60.65%), Space: 71.6 MB (80.94%) - LeetHub

---
+++ 0878-shifting-letters/0878-shifting-letters.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    string shiftingLetters(string s, vector<int>& shifts) {
+        long long maxi = 0;
+        for (int i = shifts.size() - 1; i >= 0; i--) {
+            maxi += shifts[i];
+            s[i] = 'a' + (s[i] - 'a' + maxi) % 26;
+        }return s;
+    }
+};
