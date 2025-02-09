afc32ea2e9786ba9920fb588b73d7b513001be6b
SaniyaParveenCode <168137306+SaniyaParveenCode@users.noreply.github.com>
2025-02-09 19:44:53 +0530

    Time: 72 ms (40.88%), Space: 87.6 MB (70.06%) - LeetHub

--- 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.cpp
+++ 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.cpp
@@ -3,19 +3,17 @@ public:
     long long countBadPairs(vector<int>& nums) {
         int n = nums.size();
         long long result = 0;
-
-        unordered_map<int , int > mp;
-        mp[nums[0]] = 1;
-
-        for(int j = 1;j<n;j++) {
-        int countOfNumsj = mp[nums[j] - j];
-
-        int totalNumsBeforej = j;
-
-        int badpairs = totalNumsBeforej - countOfNumsj;
-        result += badpairs;
-        mp[nums[j]- j]++;
-       }
+        unordered_map<int , int > mp;      
+        for(int j = 0;j<n;j++) {
+            int diff = nums[j] - j;
+            int goodPairs = mp[diff];
+            int totalPairsInPast = j;
+            result += totalPairsInPast - goodPairs;
+            mp[diff]++;
+        }      
        return result;
     }
 };
+
+
+
