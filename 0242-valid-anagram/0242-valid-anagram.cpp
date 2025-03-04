5a9c353ba836e36fd5c5a0eb87aea7f262ed8d57
SaniyaParveenCode <168137306+SaniyaParveenCode@users.noreply.github.com>
2025-03-04 19:24:55 +0530

    Time: 7 ms (22.4%), Space: 10 MB (11.65%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,8 +1,18 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin() , s.end());
-        sort(t.begin() , t.end());
-        return s==t;
+        if(s.size() != t.size())
+        return false;
+
+        unordered_map<char , int>mp;
+        for(char c: s) {
+          mp[c]++;
+        }
+        for(char c:t) {
+            mp[c]--;
+            if(mp[c] <0)
+            return false;
+        }
+        return true;
      }
 };
