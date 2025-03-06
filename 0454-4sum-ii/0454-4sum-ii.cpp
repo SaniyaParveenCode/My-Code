7f17a966a48e9a38d61c44226b16b4e82af03062
SaniyaParveenCode <168137306+SaniyaParveenCode@users.noreply.github.com>
2025-03-06 10:29:19 +0530

    Time: 82 ms (87.19%), Space: 27.9 MB (98.34%) - LeetHub

--- 0454-4sum-ii/0454-4sum-ii.cpp
+++ 0454-4sum-ii/0454-4sum-ii.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     int fourSumCount(vector<int>& A, vector<int>& B, vector<int>& C, vector<int>& D) {
+        unordered_map<int, int> mp;
+        int ans = 0;
 
-int ans = 0;
-unordered_map<int , int>mp;
+        for (int a : A)
+            for (int b : B)
+                mp[a + b]++;
 
-for(int a:A)
- for(int b:B)
-  mp[a+b]++;
+        for (int c : C)
+            for (int d : D)
+                if (mp.count(-(c + d)))
+                    ans += mp[-(c + d)];
 
-  for(int c:C)
-   for(int d:D)
-     ans+= mp[-(c+d)];
-
-  return ans;
-  }
-};  
+        return ans;
+    }
+};
