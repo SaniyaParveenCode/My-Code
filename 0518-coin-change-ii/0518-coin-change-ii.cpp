3b25feb734577e8e65fba0ab59809b40fa26f56b
SaniyaParveenCode <168137306+SaniyaParveenCode@users.noreply.github.com>
2025-05-29 15:07:42 +0530

    Time: 31 ms (28.34%), Space: 28.6 MB (39.51%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,22 +1,19 @@
-vector<vector<int>>dp;
 class Solution {
 public:
-  int func1( int ind , int amount , vector<int>&coins) {
-    if(amount == 0)
-    return 1;
-    if(ind<0) return 0;
-    if(dp[ind][amount] != -1)
-    return dp[ind][amount];
-    int ways = 0;
-    for(int coin_amount=0;coin_amount<=amount;
-        coin_amount+=coins[ind]) {
-        ways += func1(ind-1 , amount - coin_amount , coins);
-    }
-return dp[ind][amount] = ways;
-}
- int change(int amount, vector<int>& coins) {
-     dp = vector<vector<int>>(coins.size(), vector<int>(amount + 1, -1));
-    return func1(coins.size()-1 ,amount , coins);
+int changecoin(int i , int amount , vector<int>&coins , vector<vector<int>>&dp) {
+    if(amount == 0) return 1;
+    if(i==coins.size() || amount < 0 ) return 0;
+    if(dp[i][amount] != -1) return dp[i][amount];
 
+    int include  = 0;
+    if(coins[i] <= amount) {
+        include =  changecoin( i , amount - coins[i] , coins , dp);
     }
+int  exclude = changecoin(i+1 , amount , coins , dp);
+return dp[i][amount] = include + exclude;
+}
+int change(int amount, vector<int>& coins) {
+vector<vector<int>>dp(coins.size() , vector<int>(amount+1 , -1));
+return changecoin(0 , amount , coins , dp);
+}
 };
