7a390c16fd663dae03e6bd4a9764331c7cfefc7e
SaniyaParveenCode <168137306+SaniyaParveenCode@users.noreply.github.com>
2025-05-29 19:40:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,26 +1,21 @@
 class Solution {
-public:
-int solve(string& a , string& b , int i , int j , vector<vector<int>>&dp) {
-    if(i== a.length())  return 0;
-    if(j== b.length()) return 0;
+    public:
+    int solve(int i , int j , string text1 , string text2 , vector<vector<int>>&dp) {
+        if( i >= text1.size() || j >= text2.size() ) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
 
-    if(dp[i][j] != -1)
-    return dp[i][j];
-
-    int ans = 0;
-    if(a[i]==b[j]) {
-    ans = 1+solve(a,b,i+1,j+1 , dp);
+        if(text1[i]== text2[j]) {
+            return  dp[i][j] = 1+ solve(i+1 , j+1 , text1, text2 , dp);
+        }
+        else {
+          return  dp[i][j] = max(
+            solve(i+1 , j, text1 , text2 , dp),
+            solve(i , j+1 , text1 , text2 , dp)
+            );
+        }
     }
-
-    else 
-    {
-    ans = max(solve(a,b,i+1,j, dp) , solve(a,b,i,j+1 , dp));
+    int longestCommonSubsequence(string text1 ,string text2) {
+        vector<vector<int>>dp(text1.size() +1 , vector<int>(text2.size() +1 , -1));
+      return solve(0,0,text1, text2 , dp);
     }
-    return dp[i][j] = ans;
-}
-
-int longestCommonSubsequence(string text1, string text2) {
-    vector<vector<int>>dp(text1.length() , vector<int>(text2.length() , -1));
-    return solve(text1 , text2 ,  0,0 , dp);
-}
 };
