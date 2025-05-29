b44ccb0dabc30f8bf50855e9082cb3c23b9cf98b
SaniyaParveenCode <168137306+SaniyaParveenCode@users.noreply.github.com>
2025-05-29 18:22:44 +0530

    Time: 0 ms (100%), Space: 9.3 MB (33.24%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,27 +1,16 @@
-// class Solution {
-// public:
-//      int uniquePaths(int m, int n) {
-//         vector<vector<int>>dp(m , vector<int>(n,1));
-//         for(int i=1;i<m;i++) {
-//             for(int j=1;j<n;j++)
-//             dp[i][j] = dp[i-1][j]+ dp[i][j-1];
-//         }
-//         return dp[m-1][n-1];
-//      }
-// };
-
 class Solution {
 public:
-    int solve(int i, int j, vector<vector<int>>& dp) {
-        if (i == 0 || j == 0) return 1; 
-        if (dp[i][j] != -1) return dp[i][j]; 
-
-        dp[i][j] = solve(i - 1, j, dp) + solve(i, j - 1, dp);
-        return dp[i][j];
-    }
-
-    int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n, -1)); 
-        return solve(m-1, n-1 , dp); 
-    }
+ int solve( int i , int j , int m , int n , vector<vector<int>>&dp) {
+if( i == m-1 || j== n-1) return 1;
+if(i >= m|| j>= n ) return 0;
+if(dp[i][j] != -1) return dp[i][j];
+int take = 0;
+take = solve(i+1 , j , m , n , dp);
+int not_take = solve(i, j+1 , m, n , dp);
+return dp[i][j] = take + not_take;
+}
+int uniquePaths(int m, int n) {
+    vector<vector<int>>dp(m , vector<int>(n , -1));
+       return solve(0,0,m,n , dp);
+}
 };
