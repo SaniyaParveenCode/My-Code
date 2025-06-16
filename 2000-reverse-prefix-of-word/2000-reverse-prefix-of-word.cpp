a298147f83e4a30a62c723dcfe73e79f36a45f0b
SaniyaParveenCode <168137306+SaniyaParveenCode@users.noreply.github.com>
2025-06-16 22:40:10 +0530

    Time: 0 ms (100%), Space: 8.2 MB (98.99%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
@@ -1,13 +1,32 @@
+// class Solution {
+// public:
+//     string reversePrefix(string word, char &ch) {
+//        int  n = word.size();
+//        int index = word.find(ch);
+//        if(index != string::npos) {
+//         reverse(word.begin()  , word.begin() +index+1);
+//        }
+//        return word;
+//     }
+// };
+
 class Solution {
 public:
     string reversePrefix(string word, char &ch) {
-       int  n = word.size();
-       int index = word.find(ch);
-       if(index != string::npos) {
-        // string::npos is a special constant in C++ that represents a value which indicates "not found" in string operations
-        reverse(word.begin()  , word.begin() +index+1);
-        // word.begin() +index+1 -> Points to the character just after the index of the first occurrence of ch
-       }
-       return word;
+        int index = -1;
+
+        for(int i=0;i<word.size();i++) {
+            if(word[i]==ch) {
+                index = i;
+                break;
+
+            }
+        }
+        if(index== -1)
+        return word;
+        for (int i = 0, j = index; i <= j; i++, j--) {
+            swap(word[i] , word[j]);
+        }
+        return word;
     }
 };
