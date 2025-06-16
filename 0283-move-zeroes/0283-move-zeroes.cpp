9259e045d12092e5472c4920b0947b2fb8140a11
SaniyaParveenCode <168137306+SaniyaParveenCode@users.noreply.github.com>
2025-06-16 19:54:38 +0530

    Time: 0 ms (100%), Space: 23.9 MB (49.93%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -3,16 +3,15 @@ public:
 void moveZeroes(vector<int>& nums) {
     int n = nums.size();
     int left = 0;
-    for(int right = 0;right<n;right++)
-    {
-        if(nums[right]==0) {
-        continue;
+    for(int right = 0;right < n;right++) {
+        if(nums[right] == 0){
+            continue;
         }
         else {
             swap(nums[right] , nums[left]);
             left++;
         }
     }
-        return ;
-    }
+    return ;
+}
 };
