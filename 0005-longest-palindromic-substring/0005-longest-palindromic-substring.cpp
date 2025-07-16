5c587e014a74ec9f577934e9a2f6130dddc41069
SaniyaParveenCode <168137306+SaniyaParveenCode@users.noreply.github.com>
2025-07-16 07:10:44 +0530

    Time: 7 ms (87.59%), Space: 9.1 MB (99%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,55 +1,30 @@
 class Solution {
-public:
-int check(string s , int p1 , int p2,int &st , int & en)
-{
-    int count = 0;
-    if(p1==p2)
-    {
-        count++;
-        p1--;
-        p2++;
-    }
-    while(p1>=0 and p2 < s.length())
-    {
-        if(s[p1]!=s[p2])
-        {
-            st = p1+1;
-            en = p2-1;
-            return count;
-        }
-        else {
-            p1--;
-            p2++;
-            count+=2;
-        }
-    }
-    st = p1+1;
-    en = p2-1;
-    return count;
-}
-    string longestPalindrome(string s) {
-        int st =  -1;
-        int en = -1;
-        int len = 0;
-        int f_s = -1;
-        int f_e = -1;    
-        for(int i = 0;i <s.length();i++) 
-        {
-            int x = check(s,i,i,st,en);
-            if(x > len)
-            {
-                len = x;
-                f_s = st;
-                f_e = en;
+    public:
+string longestPalindrome(string s) {
+    int start = 0, maxLen = 0;
+
+    for (int i = 0; i < s.size(); i++) {
+        // Odd length
+        int l = i, r = i;
+        while (l >= 0 && r < s.size() && s[l] == s[r]) {
+            if (r - l + 1 > maxLen) {
+                start = l;
+                maxLen = r - l + 1;
             }
-    int y = check(s,i,i+1,st,en); 
-     if(y>len)
-     {
-        len = y;
-        f_s = st;
-        f_e = en;
+            l--; r++;
+        }
+
+        // Even length
+        l = i, r = i + 1;
+        while (l >= 0 && r < s.size() && s[l] == s[r]) {
+            if (r - l + 1 > maxLen) {
+                start = l;
+                maxLen = r - l + 1;
+            }
+            l--; r++;
+        }
     }
-}     
-return s.substr(f_s,f_e-f_s+1);    
-    }
+
+    return s.substr(start, maxLen);
+}
 };
