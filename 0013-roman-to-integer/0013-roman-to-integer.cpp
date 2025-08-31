33cc09e5f7ab5f56b31d11ae0d902205a42b651b
SaniyaParveenCode <168137306+SaniyaParveenCode@users.noreply.github.com>
2025-08-31 21:27:35 +0530

    Time: 3 ms (75.5%), Space: 10.9 MB (75.28%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,28 +1,70 @@
-// 1-> Mapping Roman to Integer:
-// Each Roman character has a specific value:
-
-// 2-> Subtraction Rule:
-
-// If the current Roman character (s[i]) is smaller than the next one (s[i+1]), subtract its value.
-
-// Otherwise, add its value.
-
-
 class Solution {
 public:
     int romanToInt(string s) {
-        int ans = 0;
-        unordered_map<char, int> mp{
-            {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, 
-            {'C', 100}, {'D', 500}, {'M', 1000}};
+        int n = s.length();
+        int num = 0;
+        for(int i = n-1;i>=0; i--) {
+            if(i>0) {
+                if(s[i-1]=='I' && s[i]=='V') 
+                {
+                    num+=4;
+                    i--;
+                    continue;
+                }
+        else if(s[i-1]=='I' && s[i]=='X') 
+                {
+                    num+=9;
+                    i--;
+                    continue;
+                }
+        else if(s[i-1]=='X' && s[i]=='L') 
+                {
+                    num+=40;
+                    i--;
+                    continue;
+                }
+        else if(s[i-1]=='X' && s[i]=='C') 
+                {
+                    num+=90;
+                    i--;
+                    continue;
+                }
+        else if(s[i-1]=='C' && s[i]=='D') 
+                {
+                    num+=400;
+                    i--;
+                    continue;
+                } 
+        else if(s[i-1]=='C' && s[i]=='M') 
+                {
+                    num+=900;
+                    i--;
+                    continue;
+                }       
 
-        for (int i = 0; i < s.size(); i++) {
-            if (i < s.size() - 1 && mp[s[i]] < mp[s[i + 1]]) {
-                ans = ans - mp[s[i]];
-            } else {
-                ans = ans + mp[s[i]];
+            }
+            if(s[i]=='I') {
+                num+=1;
+            }
+            else if(s[i]=='V') {
+                num+=5;
+            }
+            else if(s[i]=='X') {
+                num+=10;
+            }
+            else if(s[i]=='L') {
+                num+=50;
+            }
+            else if(s[i]=='C') {
+                num+=100;
+            }
+            else if(s[i]=='D') {
+                num+=500;
+            }
+            else if(s[i]=='M') {
+                num+=1000;
             }
         }
-        return ans;
+        return num;
     }
 };
