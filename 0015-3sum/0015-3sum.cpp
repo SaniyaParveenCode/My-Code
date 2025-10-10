e55ba3ca7147329e6e8652eb44ba375c7ec7382c
SaniyaParveenCode <168137306+SaniyaParveenCode@users.noreply.github.com>
2025-10-10 11:19:48 +0530

    Time: 51 ms (50.18%), Space: 29.1 MB (48.15%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,57 +1,30 @@
-// class Solution {
-// public:
-// vector<vector<int>>threeSum(vector<int>&nums) {
-//     sort(nums.begin(),nums.end());
-//     vector<vector<int>>output;
-//     for(int i=0;i<nums.size();i++) {
-//         int first = nums[i];
-//         int first = 
-//     }
-// }      
-//     }
-// };
-
-
 class Solution {
-public:
-    // Helper function to find pairs with a given target sum
-    void find2sum(vector<int>& nums, int i, int j, int target, vector<vector<int>>& ans) {
-        while (i < j) {
-            int sum = nums[i] + nums[j];
-
-            if (sum > target) {
-                j--; // Decrease the sum by moving the right pointer left
-            } else if (sum < target) {
-                i++; // Increase the sum by moving the left pointer right
-            } else {
-                ans.push_back({-target, nums[i], nums[j]}); // Found a valid triplet
-                
-                // Skip duplicates
-                while (i < j && nums[i] == nums[i + 1]) i++;
-                while (i < j && nums[j] == nums[j - 1]) j--;
-                
-                i++;
-                j--;
-            }
+ public:
+  vector<vector<int>> threeSum(vector<int>& nums) {
+    if (nums.size() < 3)
+      return {};
+    vector<vector<int>> ans;
+    ranges::sort(nums);
+    for (int i = 0; i + 2 < nums.size(); ++i) {
+      if (i > 0 && nums[i] == nums[i - 1])
+        continue;
+      int l = i + 1;
+      int r = nums.size() - 1;
+      while (l < r) {
+        const int sum = nums[i] + nums[l] + nums[r];
+        if (sum == 0) {
+          ans.push_back({nums[i], nums[l++], nums[r--]});
+          while (l < r && nums[l] == nums[l - 1])
+            ++l;
+          while (l < r && nums[r] == nums[r + 1])
+            --r;
+        } else if (sum < 0) {
+          ++l;
+        } else {
+          --r;
         }
+      }
     }
-
-    vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> ans;
-
-        int n = nums.size();
-        if (n < 3) return ans; // Not enough elements for a triplet
-
-        sort(nums.begin(), nums.end()); // Sort the array
-
-        for (int i = 0; i < n; i++) {
-            // Skip duplicates for the current element
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-
-            int target = -nums[i]; // Target sum for the two-pointer search
-            find2sum(nums, i + 1, n - 1, target, ans);
-        }
-
-        return ans;
-    }
+    return ans;
+  }
 };
